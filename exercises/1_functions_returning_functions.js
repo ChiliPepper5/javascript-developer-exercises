f7c691e8687dd3d55fa1a93b75dea6dd8f307f07
tejindersinghgill96 <138294086+tejindersinghgill96@users.noreply.github.com>
2023-07-01 18:24:36 +0530

    Update 1_functions_returning_functions.js
    javascript excercise completed

--- exercises/1_functions_returning_functions.js
+++ exercises/1_functions_returning_functions.js
@@ -7,7 +7,9 @@ parameter passed to createGetFunction when it was called.
 */
 function createGetFunction(elementToReturn) {
   // PLACE YOUR CODE BELOW
-
+    return function(array) {
+      return array[elementToReturn];
+    };
   // PLACE YOUR CODE ABOVE
 }
 
